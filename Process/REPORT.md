d63675cc7ca201c2d199ed668a489abf354566fe
Stanley Kelder <stanleykelder@gmail.com>
2017-02-02 15:36:46 +0100

    report

---
+++ Process/REPORT.md
@@ -0,0 +1,61 @@
+#Drinkgedrag Congo en Spectrum
+
+##Overzicht
+Deze visualisatie geeft inzicht in de aankopen gedaan per tablet in de kamers van de studieverenigingen Congo en Spectrum. De nested circles geven een beeld van de ondelinge verhoudingen. De line graph geeft een beeld van de aankopen per datum en de barchart geeft een beeld van de verschillen per dag van de week.
+![alt text](https://github.com/stanleykelder/Project/blob/master/doc/Barchart.png)
+
+##Design
+
+###Overzicht
+Mijn enige html-file is index.html. Deze staat in de root, zodat mijn pagina te bezichtigen is via github pages. Mijn Javascript files staan in een aparte js map, de CSS file staat in een CSS map en de twee JSONs staan in de map data. 
+
+###In detail
+####Styling
+files: styling.css
+
+Alle styling staat in 1 CSS file, met comments waar de styling voor is. Dit vond ik het meest overzichtelijk.
+
+####Top bar
+files: indes.html
+
+In de top bar staat rechts mijn naam. Links staat een 'Welkom!'-button met informatie over gebruik van de visualisatie on hover. in het midden staat een button voor Congo en een button voor Spectrum met wat informatie over de verening bij hover. Ook staat hier wat informatie over wat er uit de visualisatie te halen is. Deze top-bar is volledig gemaakt in html.
+
+####Data
+files: circle.json, payments,json, loadData.js
+
+in payments.json staat alle data. Circle.json heb ik handmatig gemaakt om makkelijk in te kunnen laden in de cirkelvisualisatie. In loadData.js wordt alle data ingeladen en in de juiste vorm gezet. Ook wordt hier de cirkelvisualisatie getekend, zodat dit altijd pas gebeurt na het inladen van alle data.
+
+####Cirkels
+files: cirles.js circle.json
+
+De data van deze visualisatie komt uit circle.json. De visualisatie zelf is geinspireerd op Mike Bostock's 'Zoomable Circle Packing' (released under the GNU General Public License, version 3). 
+
+De cirkelvisualisatie heeft zowel een hover als een klik. Bij de hover wordt de rand van de cirkel waarover wordt gehoverd zwart gemaakt, en is er in een tooltip te zien wat de naam is van de cirkel. Bij een klik wordt er ingezoomd naar de cirkel waarop wordt geklikt. Daarnaast wordt bij een klik een lijndiagram getekend met de data die horen bij de cirkel waarop is geklikt. Wordt er geklikt op een witte cirkel, dan wordt ook een staafdiagram weergegeven met de data die bij desbetreffende cirkel hoort. De witte cirkels zijn de diepste cirkels en staan voor een specifiek product.
+
+####Lijndiagram
+files: linechart.js
+
+De lijndiagram wordt gemaakt wanneer er op een cirkel wordt geklikt. Het tekenen van de lijndiagram bestaat uit 2 delen: het tekenen van de assen en het tekenen van de lijnen. De assen moeten 1 keer getekend worden en de domeinen van de assen worden bepaald circles.js en doorgegeven aan drawAxes in linechart.js. De lijnen worden apart getekend omdat het aantal lijnen afhankelijk is van de cirkel waarop wordt geklikt. 
+
+####Staafdiagram
+files: barchart.js
+
+De staafdiagram wordt gemaakt als er op een witte cirkel wordt geklikt. Daarbij heeft de barchart een listener voor de dropdown. In deze dropdown kan gekozen worden tussen de maximale, gemiddelde of totale waarde per dag. Als deze verandert wordt de staafdiagram opnieuw getekend.   
+
+###Overig
+####Kleurkeuze
+De kleuren van Congo en Spectrum zijn respectievelijk groen en blauw. Daarom heb ik ook gekozen om de visualisaties in deze kleuren te houden. Ik heb ervoor gekozen om verder alleen maar grijstinten te gebruiken. Dit vond ik het mooist overkomnen.
+
+####W3 template
+Voor de algemene css, de opmaak van de pagina, heb ik een template gebruikt van W3schools. Onderaan de pagina heb ik een vermelding hiervan laten staan. Ik heb dit in eerste instantie gebruikt zodat de grafieken niet zouden overlappen bij het inzoomen, maar dat ben ik op een gegeven moment uit het oog verloren en toen was deze functionaliteit verloren. Omdat ik het toen toch al allemaal in deze template had gemaakt, en ik het er verder mooi uit vind zien, heb ik besloten dit wel te behouden.
+
+###Tussentijdse veranderingen en beslissingen
+In eerste instantie wilde ik in de staafdiagram een visualisatie maken van de gebruikers die dingen kochten. Ik zou dan op verschillende dingen vergelijken, bijvoorbeeld op de verhouding tussen mannen en vrouwen, of op leeftijd. Ik heb uiteindelijk besloten dit niet te doen omdat het mij te veel tijd zou kosten. 
+
+###Het perfecte product
+Als ik nog meer tijd zou hebben, dan zou ik het allemaal natuurlijk nog veel mooier kunnen maken. Ten eerste zou ik dan natuurlijk wel de staafdiagram maken met vergelijkingen tussen gebruikers. Ik zou sowieso meer opties willen om te vergelijken. Staven van Congo en Spectrum per dag naast elkaar bijvoorbeeld, zodat er vergeleken kan worden wat de vershillen zijn. 
+
+Het perfecte product zou verder niet een JSON inlezen maar gekoppeld zijn aan een MongoDB database. Op deze manier zou de informatie ook nog eens live ge-update kunnen worden. De eerste week heb ik naar mogelijkheden hiervoor gekeken, maar heb ik besloten dat dit te veel tijd zou kosten om uit te zoeken.
+
+Ik heb al gehoord dat de verenigingen de visualisaties wel interessant vinden. Misschien ga ik deze punten dus nog wel verwerken in de toekomst zodat mijn visualisaties gebruit kunnen worden door de verenigingen!
+
--- js/barchart.js
+++ js/barchart.js
@@ -1,65 +1,149 @@
 var drawBarchart = function(ver, prod, type) {
 
-// format unix time to day of week
-var weekday = d3.timeFormat("%A");
+  // format unix time to day of week
+  var weekday = d3.timeFormat("%A");
 
-var svg = d3.select("#barchart"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+  var svg = d3.select("#barchart"),
+      margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
 
-// create tooltip
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
-    return "<strong><span>" + eval("d.value." + type) + "</span> </strong> ";
+  // create tooltip
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong><span>" + eval("d.value." + type) + "</span> </strong> ";
+    });
+  svg.call(tip);
+
+  // scale axes
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+  // create dataset for barhart
+  var nestdata = dates.map(function(d) {
+    return {day: weekday(d.date), amount: eval("d." + ver + "." + prod)};
   });
-svg.call(tip);
 
-// scale axes
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+  // use nest to get max, total and average value per product
+  var nest = d3.nest()
+    .key(function(d){ return d.day; })
+    .rollup(function(v) { return {
+      max: d3.max(v, function(d) { return d.amount; }),
+      total: d3.sum(v, function(d) { return d.amount; }),
+      avg: d3.mean(v, function(d) { return d.amount; })
+    }; })
+    .entries(nestdata)
 
-// create dataset for barhart
-var nestdata = dates.map(function(d) {
-  return {day: weekday(d.date), amount: eval("d." + ver + "." + prod)};
-});
+  var g = svg.append("g")
+   // format unix time to day of week
+  var weekday = d3.timeFormat("%A");
 
-// use nest to get max, total and average value per product
-var nest = d3.nest()
-  .key(function(d){ return d.day; })
-  .rollup(function(v) { return {
-    max: d3.max(v, function(d) { return d.amount; }),
-    total: d3.sum(v, function(d) { return d.amount; }),
-    avg: d3.mean(v, function(d) { return d.amount; })
-  }; })
-  .entries(nestdata)
+  var svg = d3.select("#barchart"),
+      margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // create tooltip
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong><span>" + eval("d.value." + type) + "</span> </strong> ";
+    });
+  svg.call(tip);
 
-// draw the axes
-var drawAxes = function(type) {
-    x.domain(nest.map(function(d) { return d.key; }));
-    y.domain([0, d3.max(nest, function(d) { return eval("d.value." + type); })]);
+  // scale axes
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
 
-    g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-  
-    g.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(y))
-      .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .style("text-anchor", "end")
-        .text("Aantal producten");
-  }
+  // create dataset for barhart
+  var nestdata = dates.map(function(d) {
+    return {day: weekday(d.date), amount: eval("d." + ver + "." + prod)};
+  });
+
+  // use nest to get max, total and average value per product
+  var nest = d3.nest()
+    .key(function(d){ return d.day; })
+    .rollup(function(v) { return {
+      max: d3.max(v, function(d) { return d.amount; }),
+      total: d3.sum(v, function(d) { return d.amount; }),
+      avg: d3.mean(v, function(d) { return d.amount; })
+    }; })
+    .entries(nestdata)
+
+  var g = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // draw the axes
+  var drawAxes = function(type) {
+      x.domain(nest.map(function(d) { return d.key; }));
+      y.domain([0, d3.max(nest, function(d) { return eval("d.value." + type); })]);
+
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    
+      g.append("g")
+          .attr("class", "y axis")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("fill", "#000")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "0.71em")
+          .style("text-anchor", "end")
+          .text("Aantal producten");
+    }
+
+  drawAxes(type);
+
+  // draw the bars
+  var drawBars = function(type) {
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        return "<strong><span>" + eval("d.value." + type) + "</span> </strong> ";
+      });
+    svg.call(tip);
+
+    g.selectAll(".bar")
+    .data(nest)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.key); })
+      .attr("y", function(d) { return y(eval("d.value." + type)); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(eval("d.value." + type)); })
+      .sort()
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+  }   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // draw the axes
+  var drawAxes = function(type) {
+      x.domain(nest.map(function(d) { return d.key; }));
+      y.domain([0, d3.max(nest, function(d) { return eval("d.value." + type); })]);
+
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    
+      g.append("g")
+          .attr("class", "y axis")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("fill", "#000")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "0.71em")
+          .style("text-anchor", "end")
+          .text("Aantal producten");
+    }
 
   drawAxes(type);
 
@@ -85,7 +169,7 @@ var drawAxes = function(type) {
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
   }
-  
+
   drawBars(type);
 
   // change graph when dropdown changes
@@ -96,9 +180,6 @@ var drawAxes = function(type) {
       drawAxes(newData);
       drawBars(newData);
     });
-
-  
-  
 };
 
 
